30c1a8c948a47fbab55c1e525f89c2a5c322c5eb
Isaiah Banks <isaiah.banks42@gmail.com>
2024-07-14 21:35:35 -0400

    Attempt to fix some issues. Not perfect though

--- keyboards/keychron/v6/ansi_encoder/keymaps/custom/keymap.c
+++ keyboards/keychron/v6/ansi_encoder/keymaps/custom/keymap.c
@@ -41,49 +41,123 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 #endif // ENCODER_MAP_ENABLE
 
 bool process_record_user(uint16_t keycode, keyrecord_t * record) {
-  if (record -> event.pressed) {
+    if (!snap_tap(keycode, record)) {
+        return false;
+    }
+    if (!process_record_keychron(keycode, record)) {
+        return false;
+    }
+    return true;
+}
+
+bool a_stopped = false;
+bool d_stopped = false;
+bool w_stopped = false;
+bool s_stopped = false;
+
+bool a_pressed = false;
+bool d_pressed = false;
+bool w_pressed = false;
+bool s_pressed = false;
+
+bool snap_tap(uint16_t keycode, keyrecord_t * record) {
     switch (keycode) {
         case KC_CUSTOM_A:
-            unregister_code(KC_D); // Release 'D'
-            register_code(KC_A);  // Register 'A'
+            if (record->event.pressed) {
+                a_stopped = false;
+                a_pressed = true;
+
+                unregister_code(KC_D);
+
+                if (!d_stopped) {
+                    d_stopped = true;
+                }
+
+                register_code(KC_A);
+            } else {
+               if (d_pressed && d_stopped) {
+                    register_code(KC_D);
+               }
+
+               a_pressed = false;
+               d_stopped = false;
+
+               unregister_code(KC_A);
+            }
             return false;
         case KC_CUSTOM_D:
-            unregister_code(KC_A); // Release 'A'
-            register_code(KC_D);  // Register 'D'
+            if (record->event.pressed) {
+                d_stopped = false;
+                d_pressed = true;
+
+                unregister_code(KC_A);
+
+                if (!a_stopped) {
+                    a_stopped = true;
+                }
+
+                register_code(KC_D);
+            } else {
+                if (a_pressed && a_stopped) {
+                    register_code(KC_A);
+                }
+
+                d_pressed = false;
+                a_stopped = false;
+
+                unregister_code(KC_D);
+            }
             return false;
         case KC_CUSTOM_W:
-            unregister_code(KC_S); // Release 'D'
-            register_code(KC_W);  // Register 'W'
+            if (record->event.pressed) {
+                w_stopped = false;
+                w_pressed = true;
+
+                unregister_code(KC_S);
+
+                if (!s_stopped) {
+                    s_stopped = true;
+                }
+
+                register_code(KC_W);
+            } else {
+                if (s_pressed && s_stopped) {
+                    register_code(KC_S);
+                }
+                w_pressed = false;
+                s_stopped = false;
+
+                unregister_code(KC_W);
+            }
             return false;
         case KC_CUSTOM_S:
-            unregister_code(KC_W); // Release 'D'
-            register_code(KC_S);  // Register 'S'
-            return false;
-    }
-  } else {
-    switch (keycode) {
-        case KC_CUSTOM_A:
-            unregister_code(KC_A);  // Release 'A'
-            return false;
-        case KC_CUSTOM_D:
-            unregister_code(KC_D);  // Release 'D'
-            return false;
-        case KC_CUSTOM_W:
-            unregister_code(KC_W);  // Release 'W'
-            return false;
-        case KC_CUSTOM_S:
-            unregister_code(KC_S);  // Release 'S'
-            return false;
-    }
-  }
+            if (record->event.pressed) {
+                s_stopped = false;
+                s_pressed = true;
 
-  if (!process_record_keychron(keycode, record)) {
-    return false;
-  }
+                unregister_code(KC_W);
 
-  return true;
+                if (!w_stopped) {
+                    w_stopped = true;
+                }
+
+                register_code(KC_S);
+            } else {
+                if (w_pressed && w_stopped) {
+                    register_code(KC_W);
+                }
+
+                s_pressed = false;
+                w_stopped = false;
+
+                unregister_code(KC_S);
+            }
+            return false;
+        default:
+            return true;
+    }
 }
 
 void housekeeping_task_user(void) {
-  housekeeping_task_keychron();
+    housekeeping_task_keychron();
 }
